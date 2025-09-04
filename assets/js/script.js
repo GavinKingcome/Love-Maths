8ca57e8124dc8329f96e0ef8a5b9a7e077bb344d
GavinKingcome <gavin@gavinkingcome.com>
2025-09-04 18:34:28 +0100

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,48 @@
-console.log("Connected!")  // Your current test
+// console.log("Connected!")  // Your current test
 
-function checkAnswer() {
-    console.log("User clicked answer button")  // Debug new features
-    let correctAnswer = 42;  // Define the variable with a test value
-    console.log("Correct answer is:", correctAnswer)
-}
-checkAnswer();
+// function checkAnswer() {
+//     console.log("User clicked answer button")  // Debug new features
+//     let correctAnswer = 42;  // Define the variable with a test value
+//     console.log("Correct answer is:", correctAnswer)
+// }
+// checkAnswer();
+
+// Game functions
+
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+        console.log("Sumit button clicked");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+        console.log(`${gameType} button clicked`);
+      }
+    });
+  }
+});
+
+function runGame() {}
+
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
+
+function displayDivisionQuestion() {}
